eedeb48b0fc09ba88fa84036afa76d347f392298
Tanmay102 <tanmaygupta102@gmail.com>
2024-07-24 14:44:48 -0500

    Change API Key

--- src/pages/api/traindata.js
+++ src/pages/api/traindata.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-      const response = await fetch('https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/traindata?apiKey=717f824f-af8f-4a2f-9e2b-296650e92636');
+      const response = await fetch(`https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/traindata?apiKey=${process.env.API_KEY}`);
       
       if (!response.ok) {
         throw new Error('Network response was not ok');
